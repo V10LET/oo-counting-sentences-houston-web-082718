fafda1208f8ec75f33f50a9ed37e1c9aa749a331
Violet Moon <violet.wmoon@gmail.com>
2018-09-02 00:40:50 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\!|\?/).delete_if { |w| w.length < 2 }
+    sent_split = self.split(/\.|\!|\?/).delete_if { |w| w.length < 2 }
+    sent_split.length
   end
 end
