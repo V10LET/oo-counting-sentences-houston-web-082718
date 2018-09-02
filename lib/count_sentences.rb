c57c89b751a56eeed863d6ad508f257dc9b3bfde
Violet Moon <violet.wmoon@gmail.com>
2018-09-02 00:41:59 +0000

    can call self to refer to the instance the method is being called on - self-awareness

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sent_split = self.split(/\.|\!|\?/).delete_if { |w| w.length < 2 }
+    sent_split.length
   end
 end
